f1b6fa0986b8cc95cea32dee4dce17a91127e06e
Alireza <alireza_mojtabavi@yahoo.com>
2020-08-14 17:29:24 +0430

    edit Elementary 1

--- Elementary/1- introduction- if, else/Ex_3_2barabare_ax.py
+++ Elementary/1- introduction- if, else/Ex_3_2barabare_ax.py
@@ -3,5 +3,5 @@ first = number%10
 variable = number%100
 second = variable//10
 third = number//100
-javab = 2*(first*100 + second*10 + third)
-print(javab)
+result = 2*(first*100 + second*10 + third)
+print(result)
